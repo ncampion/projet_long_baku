7ca9168d7232b59c35488161f6a1b1dd9cac918e
Manon Bos <manonbos@hotmail.fr>
2021-02-10 17:09:37 +0100

    Ajout de audioList.scss et audioDisplay.scss, debut de AudioDisplayComponent

---
+++ front/src/components/audio/AudioDisplayComponent.vue
@@ -0,0 +1,26 @@
+<style lang="scss" scoped>
+@import "@/styles/audioDisplay.scss";
+</style>
+
+
+<template>
+  <div class="audioDisplay-root-div">
+    <div class="toolbar">
+      
+    <!--TODO -->
+
+    </div>
+    <div ref="soundContainer" class="sound-container">
+
+    <!--TODO -->
+    
+    </div>
+
+    <div ref="movieContainer" class="movie-container">
+
+    <!--TODO -->
+    
+    </div>
+    
+  </div>
+</template>
