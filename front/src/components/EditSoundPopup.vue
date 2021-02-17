5cd31d4f9c16484f344dbfa58f4fd4b1c357b1b5
f0a1y <lacaille.pl@gmail.com>
2021-02-17 12:23:08 +0100

    début de fonctionnement du crop

--- front/src/components/EditSoundPopup.vue
+++ front/src/components/EditSoundPopup.vue
@@ -6,6 +6,7 @@
     <section class="modal-card-body">
       <input type="text" ref="nameSound" :value="nameSound"/>
       <button class="button" id="PlayAction" type="button" @click="playAction()">Play</button>
+      <button class="button" id="CropAction" type="button" @click="cropAndUpdate()">Crop</button>
       <div id="waveform"></div>
     </section>
     <footer class="modal-card-foot">
@@ -20,11 +21,12 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { namespace } from 'vuex-class';
 import WaveSurfer from "wavesurfer.js";
+import WSRegions from 'wavesurfer.js/dist/plugin/wavesurfer.regions.js';
 
 const ProjectNS = namespace('project');
 
 @Component
-export default class SmartphoneSynchroPopupComponent extends Vue {
+export default class EditSoundPopup extends Vue {
   @ProjectNS.Getter
   protected getAudioRecord!: any;
 
@@ -34,6 +36,7 @@ export default class SmartphoneSynchroPopupComponent extends Vue {
   public nameSound: string = "";
 
   private audioBlob: any;
+  private newAudioBlob: any;
   private waveSurfer: any;
 
   public async mounted(){
@@ -50,9 +53,25 @@ export default class SmartphoneSynchroPopupComponent extends Vue {
         barRadius: 3,
         cursorWidth: 1,
         height: 200,
-        barGap: 3
+        barGap: 3,
+        plugins: [
+            WSRegions.create({
+                regions: [
+                    {
+                        id: 1,
+                        start: 1,
+                        end: 2,
+                        color: 'hsla(400, 100%, 30%, 0.5)'
+                    },
+                ],
+                // dragSelection: {
+                //     slop: 5
+                // }
+            })
+        ]
     });
     this.waveSurfer.loadBlob(this.audioBlob);
+    //this.waveSurfer.enableDragSelection();
   }
 
   public playAction() {
@@ -67,5 +86,122 @@ export default class SmartphoneSynchroPopupComponent extends Vue {
     await this.$store.dispatch('project/changeAudioTitle', { audioId: this.id, title });
     await this.$store.dispatch('project/changeAudioSound', { audioId: this.id, sound: this.audioBlob });
   }
-}
+
+  public cropAndUpdate(){
+    this.newAudioBlob = this.crop(); 
+    console.log(this.newAudioBlob);
+    this.waveSurfer.loadBlob(this.newAudioBlob);
+  }
+
+  private crop() {
+    var start = this.waveSurfer.regions.list["1"].start;
+    var end = this.waveSurfer.regions.list["1"].end;
+
+     var originalAudioBuffer = this.waveSurfer.backend.buffer;
+
+    var lengthInSamples = Math.floor( (end - start) * originalAudioBuffer.sampleRate );
+    if (! window.OfflineAudioContext) {
+        if (! window.OfflineAudioContext) {
+            // $('#output').append('failed : no audiocontext found, change browser');
+            alert('webkit context not found')
+        }
+        window.OfflineAudioContext = window.OfflineAudioContext;
+    }
+    // var offlineAudioContext = new OfflineAudioContext(1, 2,originalAudioBuffer.sampleRate );
+    var offlineAudioContext = this.waveSurfer.backend.ac
+
+    var emptySegment = offlineAudioContext.createBuffer(
+        originalAudioBuffer.numberOfChannels,
+        lengthInSamples,
+        originalAudioBuffer.sampleRate );
+
+    var newAudioBuffer = offlineAudioContext.createBuffer(
+        originalAudioBuffer.numberOfChannels,
+        (start === 0 ? (originalAudioBuffer.length - emptySegment.length) :originalAudioBuffer.length),
+        originalAudioBuffer.sampleRate);
+
+    for (var channel = 0; channel < originalAudioBuffer.numberOfChannels;channel++) {
+
+        var new_channel_data = newAudioBuffer.getChannelData(channel);
+        var empty_segment_data = emptySegment.getChannelData(channel);
+        var original_channel_data = originalAudioBuffer.getChannelData(channel);
+
+        var before_data = original_channel_data.subarray(0, start * originalAudioBuffer.sampleRate);
+        var mid_data = original_channel_data.subarray( start * originalAudioBuffer.sampleRate, end * originalAudioBuffer.sampleRate);
+        var after_data = original_channel_data.subarray(Math.floor(end * originalAudioBuffer.sampleRate), (originalAudioBuffer.length * originalAudioBuffer.sampleRate));
+
+        empty_segment_data.set(mid_data);
+        if(start > 0){
+            new_channel_data.set(before_data);
+            new_channel_data.set(after_data,(start * newAudioBuffer.sampleRate));
+        } else {
+            new_channel_data.set(after_data);
+        }
+    }
+    
+    
+    return bufferToWave(emptySegment,0,emptySegment.length)
+    
+      }
+    
+  }
+
+  // Convert a audio-buffer segment to a Blob using WAVE representation
+  // The returned Object URL can be set directly as a source for an Auido element.
+  // (C) Ken Fyrstenberg / MIT license
+  function bufferToWave(abuffer, offset, len) {
+
+    var numOfChan = abuffer.numberOfChannels,
+        length = len * numOfChan * 2 + 44,
+        buffer = new ArrayBuffer(length),
+        view = new DataView(buffer),
+        channels = [], i, sample,
+        pos = 0;
+        
+    // write WAVE header
+    setUint32(0x46464952);                         // "RIFF"
+    setUint32(length - 8);                         // file length - 8
+    setUint32(0x45564157);                         // "WAVE"
+    
+    setUint32(0x20746d66);                         // "fmt " chunk
+    setUint32(16);                                 // length = 16
+    setUint16(1);                                  // PCM (uncompressed)
+    setUint16(numOfChan);
+    setUint32(abuffer.sampleRate);
+    setUint32(abuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+    setUint16(numOfChan * 2);                      // block-align
+    setUint16(16);                                 // 16-bit (hardcoded in this demo)
+    
+    setUint32(0x61746164);                         // "data" - chunk
+    setUint32(length - pos - 4);                   // chunk length
+    
+    // write interleaved data
+    for(i = 0; i < abuffer.numberOfChannels; i++)
+      channels.push(abuffer.getChannelData(i));
+    
+    while(pos < length) {
+      for(i = 0; i < numOfChan; i++) {             // interleave channels
+        sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
+        sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // scale to 16-bit signed int
+        view.setInt16(pos, sample, true);          // update data chunk
+        pos += 2;
+      }
+      offset++                                     // next source sample
+    }
+
+    // create Blob
+    return new Blob([buffer], {type: "audio/wav"});
+    
+    function setUint16(data) {
+      view.setUint16(pos, data, true);
+      pos += 2;
+    }
+    
+    function setUint32(data) {
+      view.setUint32(pos, data, true);
+      pos += 4;
+    }
+  }
+
+
 </script>
